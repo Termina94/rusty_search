0136853ca6eb3ddb0acc246bfcaab738c7dfd14c
Dean Revell <revell@gmail.com>
2022-05-14 01:52:25 +0100

    safely access params, remove debugger tool warnings

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,10 @@ fn run_command(args: Vec<String>) -> Result<()> {
     let empty = String::with_capacity(0);
     let command = args.get(1).unwrap_or(&empty);
     let action = args.get(2).unwrap_or(&empty);
-    let params = &args[3..];
+    let params = match args.len() >= 4 {
+        true => &args[3..],
+        false => &[] as &[String],
+    };
 
     let mut route: Box<dyn Command> = match command.as_str() {
         "manage" => Box::new(Manage::new()),
--- src/tools/debug.rs
+++ src/tools/debug.rs
@@ -1,10 +1,12 @@
 use core::fmt::Debug;
 use std::fmt::Display;
 
+#[allow(dead_code)]
 fn header() {
     println!("\n##### DEBUG #####");
 }
 
+#[allow(dead_code)]
 pub fn text<S>(text: S)
 where
     S: Into<String>,
@@ -14,6 +16,7 @@ where
     println!("{}", text);
 }
 
+#[allow(dead_code)]
 pub fn obj<T>(object: T)
 where
     T: Debug,
@@ -22,6 +25,7 @@ where
     println!("{:?}", object)
 }
 
+#[allow(dead_code)]
 pub fn text_obj<S, T>(text: S, object: T)
 where
     S: Into<String>,
